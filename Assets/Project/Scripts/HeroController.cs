d262081b89a088c4abb792a530669caab0afdf31
Andrey Viktorov <aviktorov@fatestudio.org>
2015-04-18 17:19:40 +0600

    Add hero hands

--- Assets/Project/Scripts/HeroController.cs
+++ Assets/Project/Scripts/HeroController.cs
@@ -8,16 +8,12 @@ public class HeroController : MonoBehaviour {
 	//
 	public float maxVelocity = 10.0f;
 	public float acceleration = 10.0f;
-	public float throwStrength = 10.0f;
-	
-	public Transform handsTransform = null;
 	
 	//
 	private Rigidbody cachedBody;
 	private Transform cachedCameraTransform;
-	private Rigidbody grabbedObject;
-	private Transform grabbedParent;
-	private Rigidbody detectedObject;
+	private HeroHands cachedHands;
+	private Transform cachedHandsTransform;
 	
 	//
 	private Vector2 GetInput(string horizontal,string vertical) {
@@ -28,14 +24,28 @@ public class HeroController : MonoBehaviour {
 	//
 	private void Awake() {
 		cachedBody = GetComponent<Rigidbody>();
+		cachedHands = GetComponentInChildren<HeroHands>();
 	}
 	
 	private void Start() {
+		if(!Camera.main) Debug.LogError("No main camera on scene");
+		if(!cachedHands) Debug.LogError("Handles hero, yikes");
+		
 		cachedCameraTransform = Camera.main.GetComponent<Transform>();
+		cachedHandsTransform = cachedHands.transform;
+		
+		Collider collider = GetComponent<Collider>();
+		Collider[] hands = cachedHands.GetComponentsInChildren<Collider>();
+		
+		foreach(Collider hand in hands) {
+			Physics.IgnoreCollision(collider,hand);
+		}
 	}
 	
 	private void Update() {
 		if(cachedBody == null) return;
+		if(cachedHands == null) return;
+		if(cachedCameraTransform == null) return;
 		
 		// legs & hands
 		Vector2 hands = GetInput("HandsHorizontal","HandsVertical").normalized;
@@ -45,51 +55,12 @@ public class HeroController : MonoBehaviour {
 		if(movement.sqrMagnitude > 1.0f) movement.Normalize();
 		
 		float angle = Mathf.Atan2(hands.y,hands.x) * Mathf.Rad2Deg;
-		handsTransform.eulerAngles = handsTransform.eulerAngles.WithZ(angle);
+		cachedHandsTransform.eulerAngles = cachedHandsTransform.eulerAngles.WithZ(angle);
 		
 		Vector3 newVelocity = cachedBody.velocity + movement * acceleration * Time.deltaTime;
 		newVelocity.y = 0.0f;
 		
 		if(newVelocity.sqrMagnitude > maxVelocity * maxVelocity) newVelocity = newVelocity.normalized * maxVelocity;
 		cachedBody.velocity = newVelocity.WithY(cachedBody.velocity.y);
-		
-		// grab
-		bool grabbing = Input.GetButton("Grab");
-		
-		if(grabbing && detectedObject && !grabbedObject) {
-			grabbedObject = detectedObject;
-			detectedObject = null;
-			
-			// replace to breakable joint?
-			grabbedParent = grabbedObject.transform.parent;
-			grabbedObject.isKinematic = true;
-			grabbedObject.transform.parent = handsTransform;
-		}
-		
-		if(!grabbing && grabbedObject) {
-			// replace to breakable joint?
-			grabbedObject.isKinematic = false;
-			grabbedObject.transform.parent = grabbedParent;
-			grabbedObject.AddForce(handsTransform.right * throwStrength, ForceMode.Impulse);
-			
-			grabbedObject = null;
-			grabbedParent = null;
-		}
-	}
-	
-	private void OnTriggerEnter(Collider collider) {
-		if(grabbedObject) return;
-		
-		Rigidbody body = collider.attachedRigidbody;
-		if(body == null) return;
-		if(body.isKinematic) return;
-		
-		detectedObject = body;
-	}
-	
-	private void OnTriggerExit(Collider collider) {
-		if(grabbedObject) return;
-		
-		detectedObject = null;
 	}
 }
---
+++ Assets/Project/Scripts/HeroHands.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ */
+public class HeroHands : MonoBehaviour {
+	
+	//
+	public float throwStrength = 10.0f;
+	public float throwBreakForce = 10.0f;
+	
+	//
+	private Transform cachedTransform;
+	private Rigidbody detectedObject;
+	private Rigidbody grabbedObject;
+	private FixedJoint grabbedJoint;
+	
+	//
+	private void Awake() {
+		cachedTransform = GetComponent<Transform>();
+		
+		grabbedObject = null;
+		grabbedJoint = null;
+		detectedObject = null;
+	}
+	
+	private void Update() {
+		// grab
+		if(Input.GetButtonDown("Grab") && detectedObject && !grabbedObject && !grabbedJoint) {
+			grabbedObject = detectedObject;
+			
+			grabbedJoint = gameObject.AddComponent<FixedJoint>();
+			grabbedJoint.connectedBody = grabbedObject;
+			grabbedJoint.breakForce = throwBreakForce;
+		}
+		
+		// stop grabbing if our joint was broken
+		if(grabbedObject && !grabbedJoint) {
+			grabbedObject = null;
+		}
+		
+		// throw
+		if(Input.GetButtonUp("Grab") && grabbedObject && grabbedJoint) {
+			Destroy(grabbedJoint);
+			grabbedJoint = null;
+			
+			grabbedObject.AddForce(cachedTransform.right * throwStrength, ForceMode.Impulse);
+			grabbedObject = null;
+		}
+	}
+	
+	//
+	private void OnTriggerEnter(Collider collider) {
+		Rigidbody body = collider.attachedRigidbody;
+		if(body == null) return;
+		if(body.isKinematic) return;
+		
+		detectedObject = body;
+	}
+	
+	private void OnTriggerExit(Collider collider) {
+		detectedObject = null;
+	}
+}
