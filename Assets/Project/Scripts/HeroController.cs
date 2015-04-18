ef59226a2019a19f3a452352b36c09ab1cb43ae2
Andrey Viktorov <aviktorov@fatestudio.org>
2015-04-18 15:28:21 +0600

    Add hands manipulation

--- Assets/Project/Scripts/HeroController.cs
+++ Assets/Project/Scripts/HeroController.cs
@@ -9,15 +9,16 @@ public class HeroController : MonoBehaviour {
 	public float maxVelocity = 10.0f;
 	public float acceleration = 10.0f;
 	
-	//
-	private Rigidbody cachedBody;
+	public Transform handsTransform = null;
 	
 	//
-	private Vector3 GetDirection(string horizontal, string vertical) {
-		Vector3 direction = Vector3.right * Input.GetAxis(horizontal) + Vector3.forward * Input.GetAxis(vertical);
-		if(direction.sqrMagnitude > 1.0f) direction.Normalize();
-		
-		return direction;
+	private Rigidbody cachedBody;
+	private Transform cachedCameraTransform;
+	
+	//
+	private Vector2 GetInput(string horizontal,string vertical) {
+		Vector2 input = new Vector2(Input.GetAxis(horizontal),Input.GetAxis(vertical));
+		return input;
 	}
 	
 	//
@@ -25,11 +26,23 @@ public class HeroController : MonoBehaviour {
 		cachedBody = GetComponent<Rigidbody>();
 	}
 	
+	private void Start() {
+		cachedCameraTransform = Camera.main.GetComponent<Transform>();
+	}
+	
 	private void Update() {
 		if(cachedBody == null) return;
 		
-		Vector3 movement = GetDirection("LegsHorizontal","LegsVertical");
-		Vector3 hands = GetDirection("HandsHorizontal","HandsVertical");
+		Vector2 hands = GetInput("HandsHorizontal","HandsVertical").normalized;
+		Vector2 legs = GetInput("LegsHorizontal","LegsVertical");
+		
+		Vector3 movement = cachedCameraTransform.right * legs.x + cachedCameraTransform.forward * legs.y;
+		if(movement.sqrMagnitude > 1.0f) movement.Normalize();
+		
+		float angle = Mathf.Atan2(hands.y,hands.x) * Mathf.Rad2Deg;
+		Debug.Log(string.Format("Hands: {0}, Angle: {1}",hands,angle));
+		
+		handsTransform.eulerAngles = handsTransform.eulerAngles.WithZ(angle);
 		
 		Vector3 newVelocity = cachedBody.velocity + movement * acceleration * Time.deltaTime;
 		newVelocity.y = 0.0f;
