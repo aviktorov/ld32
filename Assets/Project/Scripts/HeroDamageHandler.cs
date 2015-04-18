c87ca3616d3daced7815119cdd263d6a3e244164
Andrey Viktorov <aviktorov@fatestudio.org>
2015-04-18 17:35:30 +0600

    Add hit damage handling

---
+++ Assets/Project/Scripts/HeroDamageHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ */
+public class HeroDamageHandler : MonoBehaviour {
+	
+	//
+	public float damageVelocityTreshold = 10.0f;
+	
+	//
+	private void OnCollisionEnter(Collision collision) {
+		if(collision.relativeVelocity.magnitude < damageVelocityTreshold) return;
+		
+		Debug.Log("Ouch");
+	}
+}
