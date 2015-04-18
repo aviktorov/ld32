9915a23983df7e34c5004095795ceef16204d7b2
Andrey Viktorov <aviktorov@fatestudio.org>
2015-04-18 20:06:47 +0600

    Unify input

--- Assets/Project/Scripts/HeroHands.cs
+++ Assets/Project/Scripts/HeroHands.cs
@@ -6,10 +6,6 @@ using System.Collections;
 public class HeroHands : MonoBehaviour {
 	
 	//
-	public float throwStrength = 10.0f;
-	public float throwBreakForce = 10.0f;
-	
-	//
 	private Transform cachedTransform;
 	private Rigidbody detectedObject;
 	private Rigidbody grabbedObject;
@@ -20,13 +16,27 @@ public class HeroHands : MonoBehaviour {
 		return grabbedObject;
 	}
 	
-	public void Drop() {
-		if(grabbedJoint) {
-			Destroy(grabbedJoint);
-			grabbedJoint = null;
-		}
+	public void Drop(float throwStrength = 0.0f) {
+		if(!grabbedObject) return;
+		if(!grabbedJoint) return;
 		
+		Destroy(grabbedJoint);
+		grabbedJoint = null;
+		
+		if(throwStrength > 0.0f) grabbedObject.AddForce(cachedTransform.right * throwStrength, ForceMode.Impulse);
 		grabbedObject = null;
+	}
+	
+	public void Grab(float breakForce = Mathf.Infinity) {
+		if(detectedObject == null) return;
+		if(grabbedObject) return;
+		if(grabbedJoint) return;
+		
+		grabbedObject = detectedObject;
+		
+		grabbedJoint = gameObject.AddComponent<FixedJoint>();
+		grabbedJoint.connectedBody = grabbedObject;
+		grabbedJoint.breakForce = breakForce;
 	}
 	
 	//
@@ -41,24 +51,9 @@ public class HeroHands : MonoBehaviour {
 	//
 	private void Update() {
 		
-		// grab
-		if(Input.GetButtonDown("Grab") && detectedObject && !grabbedObject && !grabbedJoint) {
-			grabbedObject = detectedObject;
-			
-			grabbedJoint = gameObject.AddComponent<FixedJoint>();
-			grabbedJoint.connectedBody = grabbedObject;
-			grabbedJoint.breakForce = throwBreakForce;
-		}
-		
 		// stop holding if our joint was broken
 		if(grabbedObject && !grabbedJoint) {
 			grabbedObject = null;
-		}
-		
-		// throw
-		if(Input.GetButtonUp("Grab") && grabbedObject && grabbedJoint) {
-			grabbedObject.AddForce(cachedTransform.right * throwStrength, ForceMode.Impulse);
-			Drop();
 		}
 	}
 	
--- Assets/Project/Scripts/HeroInput.cs
+++ Assets/Project/Scripts/HeroInput.cs
@@ -6,8 +6,21 @@ using System.Collections;
 public class HeroInput : MonoBehaviour {
 	
 	//
+	[Header("Input")]
+	public string legsHorizontal = "LegsHorizontal";
+	public string legsVertical = "LegsVertical";
+	
+	public string handsHorizontal = "HandsHorizontal";
+	public string handsVertical = "HandsVertical";
+	
+	public string grab = "Grab";
+	
+	[Header("Physics")]
 	public float maxVelocity = 10.0f;
 	public float acceleration = 10.0f;
+	
+	public float throwStrength = 10.0f;
+	public float throwBreakForce = 10.0f;
 	
 	//
 	private Rigidbody cachedBody;
@@ -17,8 +30,7 @@ public class HeroInput : MonoBehaviour {
 	
 	//
 	private Vector2 GetInput(string horizontal,string vertical) {
-		Vector2 input = new Vector2(Input.GetAxis(horizontal),Input.GetAxis(vertical));
-		return input;
+		return new Vector2(Input.GetAxis(horizontal),Input.GetAxis(vertical));
 	}
 	
 	//
@@ -49,8 +61,8 @@ public class HeroInput : MonoBehaviour {
 		if(cachedCameraTransform == null) return;
 		
 		// legs & hands
-		Vector2 hands = GetInput("HandsHorizontal","HandsVertical").normalized;
-		Vector2 legs = GetInput("LegsHorizontal","LegsVertical");
+		Vector2 hands = GetInput(handsHorizontal,handsVertical).normalized;
+		Vector2 legs = GetInput(legsHorizontal,legsVertical);
 		
 		Vector3 movement = cachedCameraTransform.right * legs.x + cachedCameraTransform.forward * legs.y;
 		if(movement.sqrMagnitude > 1.0f) movement.Normalize();
@@ -63,5 +75,9 @@ public class HeroInput : MonoBehaviour {
 		
 		if(newVelocity.sqrMagnitude > maxVelocity * maxVelocity) newVelocity = newVelocity.normalized * maxVelocity;
 		cachedBody.velocity = newVelocity.WithY(cachedBody.velocity.y);
+		
+		// grab
+		if(Input.GetButtonDown(grab)) cachedHands.Grab(throwBreakForce);
+		if(Input.GetButtonUp(grab)) cachedHands.Drop(throwStrength);
 	}
 }
