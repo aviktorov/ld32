5500726d3a168e6fb46b11a93a3c25a1c970d47b
Andrey Viktorov <aviktorov@fatestudio.org>
2015-04-20 21:00:01 +0600

    Add debug mode for stunbars

--- Assets/Project/Scripts/UIHeroStunbar.cs
+++ Assets/Project/Scripts/UIHeroStunbar.cs
@@ -9,6 +9,7 @@ public class UIHeroStunbar : MonoBehaviour {
 	[Header("Data")]
 	public float offset = 1.5f;
 	public HeroStats hero = null;
+	public bool debug = false;
 	
 	[Header("Recovery")]
 	public Transform recoveryAreaStart = null;
@@ -18,11 +19,13 @@ public class UIHeroStunbar : MonoBehaviour {
 	//
 	private Transform cachedTransform;
 	private SpriteRenderer[] cachedRenderers;
+	private float currentDebugTime;
 	
 	//
 	private void Awake() {
 		cachedTransform = GetComponent<Transform>();
 		cachedRenderers = GetComponentsInChildren<SpriteRenderer>();
+		currentDebugTime = 0.0f;
 	}
 	
 	//
@@ -31,14 +34,22 @@ public class UIHeroStunbar : MonoBehaviour {
 		// placement
 		cachedTransform.position = hero.transform.position + Vector3.up * offset;
 		
+		currentDebugTime += Time.deltaTime;
+		while(currentDebugTime > hero.recoveryInterval) currentDebugTime -= hero.recoveryInterval;
+		
+		float progress = hero.GetRecoveryProgress();
+		if(debug) progress = hero.recoveryAnimation.Evaluate(Mathf.Clamp01(currentDebugTime / hero.recoveryInterval));
+		
 		// visibility
-		foreach(SpriteRenderer renderer in cachedRenderers) {
-			renderer.enabled = hero.IsStunned();
+		if(!debug) {
+			foreach(SpriteRenderer renderer in cachedRenderers) {
+				renderer.enabled = hero.IsStunned();
+			}
 		}
 		
 		// recovery circles
 		recoveryAreaStart.localScale = Vector3.one * hero.recoveryAreaStart;
 		recoveryAreaEnd.localScale = Vector3.one * hero.recoveryAreaEnd;
-		recoverySlider.localScale = Vector3.one * hero.GetRecoveryProgress();
+		recoverySlider.localScale = Vector3.one * (1.0f - progress);
 	}
 }
