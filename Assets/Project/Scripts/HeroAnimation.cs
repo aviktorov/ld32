5bbaae562ee97b4f7c509342b57b56932fd175cb
Andrey Viktorov <aviktorov@fatestudio.org>
2015-04-20 07:36:17 +0600

    Add stunned animation state

--- Assets/Project/Scripts/HeroAnimation.cs
+++ Assets/Project/Scripts/HeroAnimation.cs
@@ -9,6 +9,7 @@ public class HeroAnimation : MonoBehaviour {
 	private Animator cachedAnimation;
 	private Rigidbody2D cachedBody;
 	private HeroHands cachedHands;
+	private HeroStats cachedHero;
 	private HeroCollisions cachedCollision;
 	private HeroInput cachedInput;
 	
@@ -16,6 +17,7 @@ public class HeroAnimation : MonoBehaviour {
 		cachedAnimation = GetComponent<Animator>();
 		cachedBody = GetComponent<Rigidbody2D>();
 		cachedHands = GetComponent<HeroHands>();
+		cachedHero = GetComponent<HeroStats>();
 		cachedInput = GetComponent<HeroInput>();
 		cachedCollision = GetComponent<HeroCollisions>();
 	}
@@ -24,7 +26,9 @@ public class HeroAnimation : MonoBehaviour {
 		Vector3 velocity = cachedBody.velocity;
 		cachedAnimation.SetFloat("VelocityY",velocity.y);
 		cachedAnimation.SetFloat("Grabbing",cachedHands.GetGrabbedObject() ? 1.0f : 0.0f);
+		
 		cachedAnimation.SetBool("InAir",cachedCollision.InAir());
 		cachedAnimation.SetBool("Moving",cachedInput.IsMoving());
+		cachedAnimation.SetBool("Stunned",cachedHero.IsStunned());
 	}
 }
